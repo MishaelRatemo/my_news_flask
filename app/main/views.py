d137e7f34df6824c4f2d4f8ad77736cc14542f53
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-01 20:36:08 +0300

    added routes fro navigarions

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from flask import render_template,request
+from . import root
+from ..requests import get_headlines, get_sources,article_source, get_news_category
+
+@root.route('/')
+def index():
+    ''' view root page function that returns index page data'''
+    
+    # Get headlines
+    mysources = get_sources('health')
+    headlines = get_headlines()
+    print(mysources)
+    title ='News Updates'
+    return render_template('index.html', health=mysources, title =title,headlines=headlines)
+
+@root.route('/article/<int:id>')
+def article(article_id):
+    ''' function for an article to return a page details'''
+    articles = article_source(article_id)
+    return render_template('articles.html',articles=articles, id=article_id)
+        
+@root.route('/categories/<category_name>')
+def news_category(category_name):
+    ''' function and route retuningt the news categories in cartegoris page '''
+    category = get_news_category(category_name)
+    category_title = f'{category_name}'  
+    return render_template('categorys.html', title= category_title, category=category)
